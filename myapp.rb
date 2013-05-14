0d59f89d4ca2fe9d1701d79339314074d0c834bc
markusherzog <mrkshrzg@gmail.com>
2013-05-14 20:14:04 +0000

    slightly changes to way of using bundler

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,7 @@
-#!/usr/bin/ruby
+class MyApp < Sinatra::Application
+	set :bind, '0.0.0.0'
 
-# myapp.rb
-require 'sinatra'
-
-set :bind, '0.0.0.0'
-
-get '/' do
-  'Hello world!'
+	get '/' do
+		'Hello world!'
+	end
 end
